4a0073a22cb7f2e655b3a3b04ea8d1d0ee1b1ac0
James Jensen <jim@hispanola.dev1.org>
2012-12-22 15:48:43 -0700

    add test dialog that performs phone vibrate function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+/**
+ * Listener for when phonegap is ready
+ */
+$(function(){
+	$(document).on("deviceready", onDeviceReady);
+});
 
+
+/**
+ * DeviceReady event callback
+ */
+function onDeviceReady() {
+	navigator.notification.alert("Let's vibrate", vibrate(1000), "Notice", "Oh yeah!")
+	
+}
+
+/**
+ * Vibrates the phone for given number of milliseconds
+ */
+function vibrate(milliseconds) {
+	navigator.notification.vibrate(milliseconds);
+}
